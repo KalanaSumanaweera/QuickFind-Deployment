c032f4f46d176ad3a2c72cc0cc9c298391560802
Kalana Sumanaweera <ksumanaweera2000@gmail.com>
2024-11-22 02:21:21 +0530

    Fix database configuration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,16 +1,28 @@
 // backend/src/config/database.js
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'quickfind_db_dep',
+//     username: 'postgres',
+//     password: process.env.DB_PASSWORD || 'Kosgolla@2000', //Add user password after pull
+
+//     logging: false,
+// });
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'quickfind_db_dep',
-    username: 'postgres',
-    password: process.env.DB_PASSWORD || 'Kosgolla@2000', //Add user password after pull
-
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Needed for Heroku Postgres
+    },
+  },
 });
 
 module.exports = sequelize;
