733cda58146a5b9bec5bd315e9d742ac617f6d84
Kalana Sumanaweera <ksumanaweera2000@gmail.com>
2024-12-05 18:35:44 +0530

    remove the firebase ids

--- frontend/js/firebaseConfig.js
+++ frontend/js/firebaseConfig.js
@@ -6,15 +6,17 @@ import {
     signInWithPhoneNumber 
 } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
 
+require('dotenv').config(); // Load environment variables
+
 const firebaseConfig = {
-  apiKey:"get the API key from .env",
-  authDomain: "quickfind-4ff60.firebaseapp.com",
-  projectId: "quickfind-4ff60",
-  storageBucket: "quickfind-4ff60.appspot.com",
-  messagingSenderId: "810240843283",
-  appId: "1:810240843283:web:eba24e2808413a9642e9e6",
-  measurementId: "G-JYY7KG623Y"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
